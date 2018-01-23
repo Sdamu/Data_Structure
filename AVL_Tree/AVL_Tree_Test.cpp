4eaf7d05ded65d9b3ddd3df04bc7ab9347dccbd0
SDamu <shr1994@yeah.net>
2018-01-23 16:10:05 +0800

    2018.01.23

--- AVL_Tree/AVL_Tree_Test.cpp
+++ AVL_Tree/AVL_Tree_Test.cpp
@@ -3,7 +3,11 @@
 
 using namespace std;
 
-int main() {
-    cout << "Hello, World!" << std::endl;
+static int arr[] = {3,2,1,4,5,6,7,16,15,14,13,12,11,10,8,9};
+#define TBL_SIZE(a) ( (sizeof(a)) / (sizeof(a[0])) )
+
+int main()
+{
+    cout<<"测试"<<endl;
     return 0;
 }
